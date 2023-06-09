8b72958283393d06d8899cd0f082618e100ce59f
CroustibatxX <47023176+CroustibatxX@users.noreply.github.com>
2023-06-09 19:24:02 +0200

    master class

---
+++ evaluate_functions.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+def evaluate(index, fenetre, joueur):
+    match index:
+        case 0:
+            score = aggressive(fenetre, joueur)
+        case 1:
+            score = moderate(fenetre, joueur)
+        case 2:
+            score = defensive(fenetre, joueur)
+
+    return score
+
+# Fonction d'évaluation d'une fenêtre de 4 jetons
+def aggressive(fenetre, joueur):
+    score = 0
+
+    if np.count_nonzero(fenetre == joueur) == 4:
+        score += 1000
+    elif np.count_nonzero(fenetre == joueur) == 3 and np.count_nonzero(fenetre == 0) == 1:
+        score += 50
+    elif np.count_nonzero(fenetre == joueur) == 2 and np.count_nonzero(fenetre == 0) == 2:
+        score += 5
+    elif np.count_nonzero(fenetre == joueur) == 1 and np.count_nonzero(fenetre == 0) == 3:
+        score += 1
+
+    return score
+
+
+# Fonction d'évaluation d'une fenêtre de 4 jetons
+def moderate(fenetre, joueur):
+    score = 0
+    adversaire = 3 - joueur
+
+    if np.count_nonzero(fenetre == joueur) == 4:
+        score += 1000
+    elif np.count_nonzero(fenetre == joueur) == 3 and np.count_nonzero(fenetre == 0) == 1:
+        score += 5
+    elif np.count_nonzero(fenetre == joueur) == 2 and np.count_nonzero(fenetre == 0) == 2:
+        score += 2
+
+    if np.count_nonzero(fenetre == adversaire) == 3 and np.count_nonzero(fenetre == 0) == 1:
+        score -= 100
+
+    return score
+
+# Fonction d'évaluation d'une fenêtre de 4 jetons
+def defensive(fenetre, joueur):
+    score = 0
+    adversaire = 3 - joueur
+
+    if np.count_nonzero(fenetre == joueur) == 4:
+        score += 1000
+    elif np.count_nonzero(fenetre == joueur) == 3 and np.count_nonzero(fenetre == 0) == 1:
+        score += 5
+
+    if np.count_nonzero(fenetre == adversaire) == 2 and np.count_nonzero(fenetre == 0) == 2:
+        score -= 3
+    if np.count_nonzero(fenetre == adversaire) == 3 and np.count_nonzero(fenetre == 0) == 1:
+        score -= 100
+
+    return score
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-
 import numpy as np
+import evaluate_functions as eval_strategy
+
+##### Variables #####
+lignes = 6
+colonnes = 7
+strategy = [0, 0]
+currentPlayer = 1
+#####################
 
 
 class bcolors:
@@ -9,10 +15,11 @@ class bcolors:
     ENDC = '\033[0m'
 
 
+################ Gestion du jeu ##################
+
 # Fonction pour créer le plateau de jeu vide
 def creer_plateau(lignes, colonnes):
     return np.zeros((lignes, colonnes), dtype=int)
-
 
 # Fonction pour afficher la grille de jeu
 def afficher_plateau(grille):
@@ -28,7 +35,6 @@ def afficher_plateau(grille):
             ligne_affichage += "|"
         print(ligne_affichage)
     print("\n")
-
 
 # Fonction pour placer un jeton dans une colonne
 def placer_jeton(plateau, colonne, joueur):
@@ -72,133 +78,157 @@ def verifie_victoire(plateau, joueur):
     return False
 
 
-# Fonction pour exécuter le jeu
-def jouer_puissance4():
-    lignes = 6
-    colonnes = 7
-    plateau = creer_plateau(lignes, colonnes)
-    joueur = 1
-    fin_partie = False
-
-    while not fin_partie:
-        afficher_plateau(plateau)
-
-        # Demander au joueur courant de choisir une colonne
-        colonne = int(input("Joueur " + str(joueur) + ", choisissez une colonne : "))
-
-        # Placer le jeton dans la colonne choisie
-        if placer_jeton(plateau, colonne, joueur):
-            print(evaluer_position(plateau, joueur))
-            # Vérifier si le joueur courant a gagné
-            if verifie_victoire(plateau, joueur):
-                afficher_plateau(plateau)
-                print("Joueur " + str(joueur) + " a gagné !")
-                fin_partie = True
-            else:
-                # Passer au joueur suivant
-                joueur = 3 - joueur  # Alternance entre 1 et 2
-        else:
-            print("La colonne est pleine. Veuillez choisir une autre colonne.")
-
-
 # Fonction d'évaluation
 def evaluer_position(plateau, joueur):
+    global currentPlayer
     score = 0
-
     # Évaluation des lignes
     for ligne in range(len(plateau)):
         for colonne in range(len(plateau[0]) - 3):
             fenetre = plateau[ligne][colonne:colonne + 4]
-            score += evaluer_fenetre(fenetre, joueur)
+            score += eval_strategy.evaluate(strategy[currentPlayer - 1], fenetre, joueur)
 
     # Évaluation des colonnes
     for colonne in range(len(plateau[0])):
         for ligne in range(len(plateau) - 3):
             fenetre = plateau[ligne:ligne + 4, colonne]
-            score += evaluer_fenetre(fenetre, joueur)
+            score += eval_strategy.evaluate(strategy[currentPlayer - 1], fenetre, joueur)
 
     # Évaluation des diagonales ascendantes
     for ligne in range(len(plateau) - 3):
         for colonne in range(len(plateau[0]) - 3):
             fenetre = [plateau[ligne + i][colonne + i] for i in range(4)]
-            score += evaluer_fenetre(fenetre, joueur)
+            score += eval_strategy.evaluate(strategy[currentPlayer - 1], fenetre, joueur)
 
     # Évaluation des diagonales descendantes
     for ligne in range(3, len(plateau)):
         for colonne in range(len(plateau[0]) - 3):
             fenetre = [plateau[ligne - i][colonne + i] for i in range(4)]
-            score += evaluer_fenetre(fenetre, joueur)
+            score += eval_strategy.evaluate(strategy[currentPlayer - 1], fenetre, joueur)
 
     return score
 
+# Fonction pour obtenir les colonnes valides
+def get_colonnes_valides(plateau):
+    colonnes_valides = []
+    for colonne in range(colonnes):
+        if plateau[0][colonne] == 0:
+            colonnes_valides.append(colonne)
+    return colonnes_valides
 
-# Fonction d'évaluation d'une fenêtre de 4 jetons
-def evaluer_fenetre(fenetre, joueur):
-    score = 0
-    adversaire = 3 - joueur
 
-    if np.count_nonzero(fenetre == joueur) == 4:
-        score += 100
-    elif np.count_nonzero(fenetre == joueur) == 3 and np.count_nonzero(fenetre == 0) == 1:
-        score += 5
-    elif np.count_nonzero(fenetre == joueur) == 2 and np.count_nonzero(fenetre == 0) == 2:
-        score += 2
 
-    if np.count_nonzero(fenetre == adversaire) == 3 and np.count_nonzero(fenetre == 0) == 1:
-        score -= 4
-
-    return score
 
 
 def minimax(plateau, profondeur, joueur):
-    eval, action = joueur_max(plateau, profondeur, joueur)
-    return action
+    score, action = joueur_max(plateau, profondeur, joueur)
+    return score
 
 
-def joueur_max(n, p, joueur):
-    if verifie_victoire(n, joueur) or p == 0:
-        return evaluer_position(n, 2), None
-    u = -math.inf
-    alpha = None
-    for c in range(7):
-        if placer_jeton(n, c, joueur):
-            v, _ = joueur_min(n, p - 1, joueur)
-            if v > u:
-                u = v
-                alpha = c
-            n[c][len(n[c]) - 1] = 0
-    return u, alpha
+def joueur_max(plateau, profondeur, joueur, alpha=None, beta=None):
+    colonnes_valides = get_colonnes_valides(plateau)
+    if profondeur == 0 or verifie_victoire(plateau, 1) or verifie_victoire(plateau, 2) or len(colonnes_valides) == 0:
+        # Si la profondeur maximale est atteinte ou si le jeu est terminé, retourner le score de la position
+        score = evaluer_position(plateau, joueur)
+        return score, None
+
+    best_score = float("-inf")
+    action = None
+    for colonne in colonnes_valides:
+        nouveau_plateau = plateau.copy()
+        placer_jeton(nouveau_plateau, colonne, joueur)
+        score, _ = joueur_min(plateau, profondeur - 1, 3 - joueur)
+        if score > best_score:
+            best_score = score
+            action = colonne
+        if alpha is not None and beta is not None:
+            if best_score >= beta:
+                return best_score, action
+            alpha = max(alpha, best_score)
+    return best_score, action
 
 
-def joueur_min(n, p, joueur):
-    if verifie_victoire(n, joueur) or p == 0:
-        return evaluer_position(n, 1), None
-    u = math.inf
-    alpha = None
-    for c in range(7):
-        if placer_jeton(n, c, joueur):
-            v, _ = joueur_max(n, p - 1, joueur)
-            if v < u:
-                u = v
-                alpha = c
-            n[c][len(n[c]) - 1] = 0
-    return u, alpha
+def joueur_min(plateau, profondeur, joueur, alpha=None, beta=None):
+    colonnes_valides = get_colonnes_valides(plateau)
+    if profondeur == 0 or verifie_victoire(plateau, 1) or verifie_victoire(plateau, 2) or len(colonnes_valides) == 0:
+        # Si la profondeur maximale est atteinte ou si le jeu est terminé, retourner le score de la position
+        score = evaluer_position(plateau, 3 - joueur)
+        return score, None
+
+    best_score = float("inf")
+    action = None
+    for colonne in colonnes_valides:
+        nouveau_plateau = plateau.copy()
+        placer_jeton(nouveau_plateau, colonne, joueur)
+        score, _ = joueur_max(plateau, profondeur - 1, 3 - joueur)
+        if score < best_score:
+            best_score = score
+            action = colonne
+        if alpha is not None and beta is not None:
+            if best_score <= alpha:
+                return best_score, action
+            beta = min(beta, best_score)
+
+    return best_score, action
 
 
-def jouer_puissance4_avec_IA():
-    lignes = 6
-    colonnes = 7
+#######################################################################################
+
+def alphabeta(plateau, profondeur, joueur):
+    score, action = joueur_max(plateau, profondeur, joueur, float("-inf"), float("inf"))
+    return score
+
+
+def get_best_move(plateau, profondeur, joueur, algo):
+    colonnes_valides = get_colonnes_valides(plateau)
+    meilleur_score = float("-inf")
+    meilleur_coup = colonnes_valides[0]
+    for colonne in colonnes_valides:
+        nouveau_plateau = plateau.copy()
+        placer_jeton(nouveau_plateau, colonne, joueur)
+        match algo:
+            case 1:
+                score = minimax(nouveau_plateau, profondeur, joueur)
+            case 2:
+                score = alphabeta(nouveau_plateau, profondeur, joueur)
+            case 3:
+                print("NON IMPLEMENT nullos")
+        if score > meilleur_score:
+            meilleur_score = score
+            meilleur_coup = colonne
+    return meilleur_coup
+
+
+
+#################### Gestion affichage ############################
+def agentNameFromIndex(playerNumber, agents, layers):
+    playerNumber = playerNumber - 1
+    match agents[playerNumber]:
+        case 0:
+            name = "Joueur"
+        case 1:
+            name = "Minimax profondeur " + str(layers[playerNumber])
+        case 2:
+            name = "Alpha-Beta profondeur " + str(layers[playerNumber])
+        case 3:
+            name = "MCTS profondeur " + str(layers[playerNumber])
+    return name
+
+
+def play(agents, layers):
     plateau = creer_plateau(lignes, colonnes)
+    global currentPlayer
     joueur = 1
     fin_partie = False
 
     while not fin_partie:
+        print(agentNameFromIndex(joueur, agents, layers))
         afficher_plateau(plateau)
 
-        if joueur == 1:
-            colonne = obtenir_meilleur_coup(plateau, 3)
+        if agents[joueur - 1] == 0:
+            colonne = int(input("Joueur " + str(joueur) + ", choisissez une colonne : ")) - 1
         else:
-            colonne = obtenir_meilleur_coup(plateau, 5)  # Profondeur de recherche pour l'IA
+            colonne = get_best_move(plateau, layers[joueur - 1], joueur, agents[joueur - 1])
 
         if placer_jeton(plateau, colonne, joueur):
             if verifie_victoire(plateau, joueur):
@@ -211,52 +241,32 @@ def jouer_puissance4_avec_IA():
                 fin_partie = True
             else:
                 joueur = 3 - joueur
+                currentPlayer = joueur
         else:
             print("La colonne est pleine. Veuillez choisir une autre colonne.")
 
 
-def obtenir_meilleur_coup(plateau, profondeur):
-    colonnes_valides = get_colonnes_valides(plateau)
-    meilleur_score = float("-inf")
-    meilleur_coup = colonnes_valides[0]
-    for colonne in colonnes_valides:
-        nouveau_plateau = plateau.copy()
-        placer_jeton(nouveau_plateau, colonne, 1)
-        score = minimax(nouveau_plateau, profondeur - 1, 2)
-        if score > meilleur_score:
-            meilleur_score = score
-            meilleur_coup = colonne
-    return meilleur_coup
+def main():
+    print("Connect 4")
+    print("_________________________")
+    print("0: Joueur physique\n1: Minimax\n2: AlphaBeta\n3: MCTS")
+    print("_________________________")
+    agent1 = int(input("Type de joueur 1 : "))
+    agent2 = int(input("Type de joueur 2 : "))
+
+    if agent1 == 1 or agent1 == 2 or agent1 == 3:
+        layer1 = int(input("Profondeur IA 1: "))
+        print("0: Agressive |  1: Modérée |  2: Défensive")
+        strategy[0] = int(input("Stratégie IA 1: "))
+    if agent2 == 1 or agent2 == 2 or agent2 == 3:
+        layer2 = int(input("Profondeur IA 2: "))
+        print("0: Agressive |  1: Modérée |  2: Défensive")
+        strategy[1] = int(input("Stratégie IA 2: "))
+
+    play([agent1, agent2], [layer1, layer2])
 
 
-# Fonction pour obtenir les colonnes valides
-def get_colonnes_valides(plateau):
-    colonnes_valides = []
-    for colonne in range(len(plateau[0])):
-        if plateau[0][colonne] == 0:
-            colonnes_valides.append(colonne)
-    return colonnes_valides
+# main()
+strategy = [1, 2]
+play([2, 2], [1, 3])
 
-
-def play():
-    print("Choix du mode de jeu :", "- J vs J : 1", "- J vs IA : 2", "- IA vs IA")
-    choix = int(input("Votre choix : "))
-    match choix:
-        case "0":
-            jouer_puissance4()
-        case "1":
-            jouer_puissance4_avec_IA()
-        case "2":
-            print("NON IMPLEMENT nullos")
-        case _:
-            print("Ce choix n'existe pas, recommencez..........")
-
-
-# Lancer le jeu
-# jouer_puissance4()
-
-# Lancer le jeu avec l'IA utilisant Alpha-Beta
-# jouer_puissance4_avec_IA_alpha_beta()
-
-# Lancer le jeu avec l'IA
-jouer_puissance4_avec_IA()
